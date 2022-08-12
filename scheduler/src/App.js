54af2369adcf37f9671f145087a238bdb9cc0e44
jho951 <jho951@naver.com>
2022-08-12 13:43:44 +0900

    #2 초기 설정

---
+++ scheduler/src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import Detail from "./components/detail/Detail";
+
+function App() {
+  return <div className="App">
+    <Detail/>
+  </div>;
+}
+
+export default App;
---
+++ scheduler/src/components/detail/Detail.js
@@ -0,0 +1,6 @@
+import React from "react";
+export default function Detail(){
+    return(
+        <div>안녕</div>
+    )
+}
