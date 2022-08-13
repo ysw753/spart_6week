159a5129e8c215771aecfd893b58d18597d26e8e
jho951 <jho951@naver.com>
2022-08-13 17:44:30 +0900

    #2 디테일 페이지 수정
    수정

--- scheduler/src/components/detail/Detail.js
+++ scheduler/src/components/detail/Detail.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import Header from "../Header/Header";
+import Modal from "./Modal";
+import { useState } from "react";
 
 const DUMMY = [
   {
@@ -28,61 +30,69 @@ const DUMMY = [
       },
     },
   },
-  {
-    user: {
-      userid: "useridawdxxa",
-      nickname: "yoo",
-      password: "12345",
-      username: "유성원",
-      day: {
-        dayId: "dayidakxxx",
-        text: "집 주변 10바퀴 걷기",
-        title: "운동하기",
-      },
-    },
-  },
-  {
-    user: {
-      userid: "useridawdxxa",
-      nickname: "yoo",
-      password: "12345",
-      username: "유성원",
-      day: {
-        dayId: "dayidakxxx",
-        text: "집 주변 10바퀴 걷기",
-        title: "운동하기",
-      },
-    },
-  },
-  {
-    user: {
-      userid: "useridawdxxa",
-      nickname: "yoo",
-      password: "12345",
-      username: "유성원",
-      day: {
-        dayId: "dayidakxxx",
-        text: "집 주변 10바퀴 걷기",
-        title: "운동하기",
-      },
-    },
-  },
 ];
 
 const Detail = () => {
+  const [uploadSchedule,setUploadSchedule]=useState(false);
+  const closeScheduleModal=()=>{
+    setUploadSchedule(false);
+  };
+
+  const [schedule,setSchedule]=useState(false);
+  const closeSchedule=()=>{
+    setSchedule(false);
+  };
+
+  const cardData= (item)=>{
+    setSchedule(true);
+    setcardD({title:item.user.day.title,contents:item.user.day.text})
+  }
+  const [cardD,setcardD]=useState({
+    title:'',
+    contents:''
+  })
+  const [edit,setedit]=useState(false);
+
+
   return (
     <>
-      <Header />
+<Modal visible={uploadSchedule} closeModal={closeScheduleModal}>
+  <h2 style={{textAlign:"center"}}>일정을 적어주세요!</h2>
+  <div>
+  <h4>제목</h4>
+  <input name="title" type={'text'} placeholder={'제목'}></input>
+  <h5>내용</h5>
+  <input name="contents"type={'text'}placeholder={'내용'}></input>
+  </div>
+  <div>
+  <Button>등록하기</Button>
+  <Button onClick={closeScheduleModal}>취소</Button>
+  </div>
+</Modal>
+
+<Modal visible={schedule} closeModal={closeSchedule}data={cardD}>
+              {false?<p>{cardD.title}</p>:<input type={'text'}></input>}
+              <p>{cardD.contents}</p>
+            <Button>수정하기</Button>
+            <Button onClick={closeSchedule}>취소</Button>
+      </Modal>
+    <Header/>
       <MainBox>
         <DaySection>
           {DUMMY.map((dayitem) => (
-            <Card>
+         
+            <Card onClick={()=>cardData(dayitem)}>
               <p>{dayitem.user.day.title}</p>
               <p>{dayitem.user.day.text}</p>
-            </Card>
-          ))}
+            </Card> 
+            ))}
+      
+        
+          
         </DaySection>
-      </MainBox>
+        <Button onClick={()=>{setUploadSchedule(true);}}>일정 등록</Button>
+        </MainBox>
+        
     </>
   );
 };
@@ -90,7 +100,6 @@ export default Detail;
 const MainBox = styled.div`
   width: 1000px;
   min-width: 1000px;
-
   background-color: orange;
   margin: auto;
 `;
@@ -123,3 +132,20 @@ const MonthSection = styled.div`
   height: 300px;
   background-color: blue;
 `;
+const Button = styled.button`
+  display : block;
+  margin:auto;
+  padding-top: 10px;
+  padding-bottom:10px;
+  padding-left:30px;
+  padding-right:30px;
+  border-radius: 7px;
+  background-color:white;
+  font-size:13px;
+  border: 3px solid gainsboro;
+  cursor: pointer;
+  &:hover {
+  text-decoration:underline;
+  background-color:gainsboro;
+    }
+`
---
+++ scheduler/src/components/detail/Modal.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+
+function Modal({ visible, closeModal,data,children}) {
+    console.log(data);
+  return (
+    <div className="Modal">
+      <ModalWrap visible={visible}onClick={closeModal}></ModalWrap>
+      <ModalBody visible={visible}>{children}</ModalBody>
+    </div>
+  );
+}
+
+export default Modal;
+
+const ModalWrap=styled.div`
+width: 100%;
+height: 100%;
+display: ${(props) => (props.visible ? "block" : "none")};
+position: fixed;
+top: 0;
+background-color: rgba(0, 0, 0, 0.8);
+z-index: 99;
+cursor: pointer;
+`
+const ModalBody = styled.div`
+display: ${(props) => (props.visible ? "block" : "none")};
+position: fixed;
+top: 30%;
+left: 50%;
+transform: translate(-50%, -50%);
+box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.5);
+background-color: white;
+border-radius: 10px;
+width: 600px;
+height: 280px;
+padding-left:10px;
+z-index: 100;
+`;
