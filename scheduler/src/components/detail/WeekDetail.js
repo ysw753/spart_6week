b036e58b2aa2452d4196f87603fb75efd5c3b0a2
ysw753 <swyoo753@naver.com>
2022-08-17 11:06:18 +0900

    장호님 weekDetail 에 함수추가

--- scheduler/src/components/detail/WeekDetail.js
+++ scheduler/src/components/detail/WeekDetail.js
@@ -9,7 +9,6 @@ import AuthContext from "../../contextStore/auth-context";
 
 const mapFnc = () => {};
 
-
 const WeekDetail = () => {
   const [weekSchedule, setweekSchedule] = useState(false);
   const closeSchedule = () => {
@@ -18,103 +17,218 @@ const WeekDetail = () => {
   const [toggle, setToggle] = useState(false);
 
   const onClickHandler = () => {
-    setToggle(toggle => !toggle)
+    setToggle((toggle) => !toggle);
   };
-
 
   const weekTitleRef = useRef();
   const weekContentsRef = useRef();
 
-  // const onCreateHandler = async () => {
-  //   const title = weekTitleRef.current.id;
-  //   const contents = weekContentsRef.current.id;
-  //   const data = { title, contents };
-    
+  //데이터 불러오기
+  const [fetchWeekData, setFetchWeedData] = useState([]);
+  const [changeState, setChangeState] = useState(false);
+
+  //get
+  const getFetch = async () => {
+    axios
+      .get(
+        "url",
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        },
+        { withCredentials: true }
+      )
+      .then((res) => {
+        const getfetchdata = res.data.daylist;
+        console.log(res.data.daylist);
+        setFetchWeedData(getfetchdata);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  //create
+  const createFetch = axios.create({
+    BASE_URL: "",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+    withCredentials: true,
+  });
+
+  const onCreateHandler = async () => {
+    const title = titleRef.current.value;
+    const contents = contentsRef.current.value;
+    const data = { title, contents };
+    try {
+      const res = await createFetch.post("", JSON.stringify(data));
+      //성공하면 우리 데이터에 저장을 시켜줘야함
+      //changeState변화줌
+      setChangeState((prev) => !prev);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-  <>
-    <Header />
+    <>
+      <Header />
       <Modal visible={weekSchedule} closeModal={closeSchedule}>
         <h1>일정을 등록하세요</h1>
-        <div style={{ display: 'flex', justifContent: 'center'}}>
-        <input type="radio" id="sun" name="day" value="sun"></input>
-          <label for="sun">일요일</label><br></br>
-        <input type="radio" id="mon" name="day" value="mon"></input>
-          <label for="mon">월요일</label><br></br>
-        <input type="radio" id="tue" name="day" value="tue"></input>
-          <label for="tue">화요일</label><br></br>
-       <input type="radio" id="wed" name="day" value="wed"></input>
-          <label for="wed">수요일</label><br></br>
-       <input type="radio" id="thr" name="day" value="thu"></input>
-          <label for="thu">목요일</label><br></br>
-       <input type="radio" id="fri" name="day" value="fri"></input>
-          <label for="fri">금요일</label><br></br>
-       <input type="radio" id="sat" name="day" value="sat"></input>
-          <label for="sat">토요일</label><br></br>
-       </div>
-        제목 <input 
-            ref={weekTitleRef}
-            name="title"
-            type={"text"}
-            placeholder={"제목"}>
-            </input>
-        내용 <input
-            ref={weekContentsRef}
-            name="contents"
-            type={"text"}
-            placeholder={"내용"}>
-            </input>
-          <div>
+        <div style={{ display: "flex", justifContent: "center" }}>
+          <input type="radio" id="sun" name="day" value="sun"></input>
+          <label for="sun">일요일</label>
+          <br></br>
+          <input type="radio" id="mon" name="day" value="mon"></input>
+          <label for="mon">월요일</label>
+          <br></br>
+          <input type="radio" id="tue" name="day" value="tue"></input>
+          <label for="tue">화요일</label>
+          <br></br>
+          <input type="radio" id="wed" name="day" value="wed"></input>
+          <label for="wed">수요일</label>
+          <br></br>
+          <input type="radio" id="thr" name="day" value="thu"></input>
+          <label for="thu">목요일</label>
+          <br></br>
+          <input type="radio" id="fri" name="day" value="fri"></input>
+          <label for="fri">금요일</label>
+          <br></br>
+          <input type="radio" id="sat" name="day" value="sat"></input>
+          <label for="sat">토요일</label>
+          <br></br>
+        </div>
+        제목{" "}
+        <input
+          ref={weekTitleRef}
+          name="title"
+          type={"text"}
+          placeholder={"제목"}
+        ></input>
+        내용{" "}
+        <input
+          ref={weekContentsRef}
+          name="contents"
+          type={"text"}
+          placeholder={"내용"}
+        ></input>
+        <div>
           <Button>등록하기</Button>
           <Button onClick={closeSchedule}>취소</Button>
-          </div>
+        </div>
       </Modal>
 
-        <WeekView>    
-        <p style={{fontSize:'30px',textAlign:'center'}}>week page</p>
+      <WeekView>
+        <p style={{ fontSize: "30px", textAlign: "center" }}>week page</p>
         <WeekBox>
-        <Day>일
-        <VscEdit size='15'style={{cursor: 'pointer'}}onClick={()=>{onClickHandler()}}/>
-        {toggle?( <P>title</P>):(<input id="sun" type='text'></input>)}
-        {toggle?( <P>content</P>):(<input id="sun" type='text'></input>)}
-        <div style={{margin:'autopx'}}>삭제하기</div>
-        </Day>
-        <Day>월
-        <VscEdit size='15'style={{cursor: 'pointer'}}onClick={()=>{onClickHandler()}}/>
-        {toggle?( <P>title</P>):(<input id="mon" type='text'></input>)}
-        {toggle?( <P>content</P>):(<input id="mon" type='text'></input>)}
-        </Day>
-        <Day>화
-        <VscEdit size='15'style={{cursor: 'pointer'}}onClick={()=>{onClickHandler()}}/>
-        {toggle?( <P>title</P>):(<input id="tue" type='text'></input>)}
-        {toggle?( <P>content</P>):(<input id="tue" type='text'></input>)}
-        </Day>
-        <Day>수
-        <VscEdit size='15'style={{cursor: 'pointer'}}onClick={()=>{onClickHandler()}}/>
-        {toggle?( <P>title</P>):(<input id="wed" type='text'></input>)}
-        {toggle?( <P>content</P>):(<input id="wed" type='text'></input>)}
-        </Day>
-        <Day>목
-        <VscEdit size='15'style={{cursor: 'pointer'}}onClick={()=>{onClickHandler()}}/>
-        {toggle?( <P>title</P>):(<input id="thu"type='text'></input>)}
-        {toggle?( <P>content</P>):(<input id="thu" type='text'></input>)}
-        </Day>
-        <Day>금
-        <VscEdit size='15'style={{cursor: 'pointer'}}onClick={()=>{onClickHandler()}}/>
-        {toggle?( <P>title</P>):(<input id="fri" type='text'></input>)}
-        {toggle?( <P>content</P>):(<input id="fri" type='text'></input>)}
-        </Day>
-        <Day>토
-        <VscEdit size='15'style={{cursor: 'pointer'}}onClick={()=>{onClickHandler()}}/>
-        {toggle?( <P>title</P>):(<input id="sat" type='text'></input>)}
-        {toggle?( <P>content</P>):(<input id="sat" type='text'></input>)}
-        </Day>
+          <Day>
+            일
+            <VscEdit
+              size="15"
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                onClickHandler();
+              }}
+            />
+            {toggle ? <P>title</P> : <input id="sun" type="text"></input>}
+            {toggle ? <P>content</P> : <input id="sun" type="text"></input>}
+            <div style={{ margin: "autopx" }}>삭제하기</div>
+          </Day>
+          <Day>
+            월
+            <VscEdit
+              size="15"
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                onClickHandler();
+              }}
+            />
+            {toggle ? <P>title</P> : <input id="mon" type="text"></input>}
+            {toggle ? <P>content</P> : <input id="mon" type="text"></input>}
+          </Day>
+          <Day>
+            화
+            <VscEdit
+              size="15"
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                onClickHandler();
+              }}
+            />
+            {toggle ? <P>title</P> : <input id="tue" type="text"></input>}
+            {toggle ? <P>content</P> : <input id="tue" type="text"></input>}
+          </Day>
+          <Day>
+            수
+            <VscEdit
+              size="15"
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                onClickHandler();
+              }}
+            />
+            {toggle ? <P>title</P> : <input id="wed" type="text"></input>}
+            {toggle ? <P>content</P> : <input id="wed" type="text"></input>}
+          </Day>
+          <Day>
+            목
+            <VscEdit
+              size="15"
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                onClickHandler();
+              }}
+            />
+            {toggle ? <P>title</P> : <input id="thu" type="text"></input>}
+            {toggle ? <P>content</P> : <input id="thu" type="text"></input>}
+          </Day>
+          <Day>
+            금
+            <VscEdit
+              size="15"
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                onClickHandler();
+              }}
+            />
+            {toggle ? <P>title</P> : <input id="fri" type="text"></input>}
+            {toggle ? <P>content</P> : <input id="fri" type="text"></input>}
+          </Day>
+          <Day>
+            토
+            <VscEdit
+              size="15"
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                onClickHandler();
+              }}
+            />
+            {toggle ? <P>title</P> : <input id="sat" type="text"></input>}
+            {toggle ? <P>content</P> : <input id="sat" type="text"></input>}
+          </Day>
         </WeekBox>
-        <div style={{marginLeft:'700px'}}>
-        <Button onClick={()=>{setweekSchedule(true)}}>일정 등록</Button>
+        <div style={{ marginLeft: "700px" }}>
+          <Button
+            onClick={() => {
+              setweekSchedule(true);
+            }}
+          >
+            일정 등록
+          </Button>
         </div>
-        
-        {toggle?(null):(<Button style={{marginLeft:'700px',marginTop:'30px'}} onClick={{}}>수정 완료</Button>)}
-  
+
+        {toggle ? null : (
+          <Button
+            style={{ marginLeft: "700px", marginTop: "30px" }}
+            onClick={{}}
+          >
+            수정 완료
+          </Button>
+        )}
       </WeekView>
     </>
   );
@@ -127,26 +241,25 @@ const WeekView = styled.div`
   width: 1500px;
   height: 800px;
   background-color: navajowhite;
-  border-radius:50px;
+  border-radius: 50px;
 `;
 
 const Day = styled.div`
   border: 2px solid saddlebrown;
-  height:450px;
-  width:200px;
-  margin:auto;
-  font-size:20px;
-  border-radius:10px;
-  font-weight:bold;
+  height: 450px;
+  width: 200px;
+  margin: auto;
+  font-size: 20px;
+  border-radius: 10px;
+  font-weight: bold;
   text-align: center;
 `;
-const WeekBox= styled.div`
+const WeekBox = styled.div`
   display: flex;
   justify-content: center;
-
-`
+`;
 const Button = styled.button`
-  position : absoulte;
+  position: absoulte;
   margin: auto;
   paddingn: autopx;
   padding-bottom: 10px;
@@ -162,7 +275,7 @@ const Button = styled.button`
     background-color: peru;
   }
 `;
-const P= styled.div`
+const P = styled.div`
 marginn:9auto
 text-align:left;
-`
+`;
